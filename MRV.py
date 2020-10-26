f150913f617c67ccd6b36a0e5c6cf385a1328707
AntonOsadchiy <1voicev1@gmail.com>
2020-10-26 17:22:41 +0200

    1.001

--- MRV.py
+++ MRV.py
@@ -39,19 +39,32 @@ def cspUtil(graph, graph_copy, vertices, k, domain, constraint):
         print("\n")
         return graph_copy
     for i in domain:
-        if not constraint(graph_copy.getNeighbours(vertices[k]), i):
+        #vertices = getSortedVertices(graph)
+
+        if not constraint(graph_copy.getNeighbours(vertices[0]), i):
             if DONE:
                 return graph_copy
-
             print(str(vertices[k]) + "move to", str(i))
-            changeTime(graph_copy, vertices[k], i)
+            changeTime(graph_copy, vertices[0], i)
 
-            return cspUtil(graph, graph_copy, vertices, k + 1, domain, constraint)
+            return cspUtil(graph, graph_copy, getSortedVertices(graph), k + 1, domain, constraint)
     
 
+<<<<<<< Updated upstream
+=======
+def adjNotAssigned(graph, lesson):
+    adj = graph.getNeighbours(lesson)
+    for l in adj:
+        if l.time != "none":
+            adj.remove(l)
+    return adj
+
+
+>>>>>>> Stashed changes
 def getSortedVertices(graph):
     vertices = graph.getVertices()
-    vertices.sort(key=lambda x: graph.degree(x), reverse=True)
+    #vertices.sort(key=lambda x: graph.degree(x), reverse=True)
+    vertices.sort(key=lambda x: len(adjNotAssigned(graph, x)), reverse=True)
     return vertices
 
 
@@ -65,16 +78,16 @@ if __name__ == "__main__":
 
     f1 = Model.Faculty("F1", ["8:30", "10:00", "11:40"], ["8:30", "10:00", "13:30", "15:00"],
                        ["10:00", "11:40", "13:30"], ["11:40", "13:30"], ["10:00", "11:40", "13:30"])
-    l1 = Model.Lesson("l1", "11:40", Model.Teacher("Jett"), ["s2", "s222", "s3333"], True)
-    l2 = Model.Lesson("l2", "10:00", Model.Teacher("Brimstone"), ["s134", "s2", "s6"], False)
-    l3 = Model.Lesson("l3", "11:40", Model.Teacher("Raze"), ["s1", "s22", "s5"], True)
-    l4 = Model.Lesson("l4", "13:30", Model.Teacher("Sage"), ["s1", "s22", "s4"], False)
-    l5 = Model.Lesson("l5", "8:30", Model.Teacher("Sage"), ["s111", "s2", "s3"], True)
-    l6 = Model.Lesson("l6", "11:40", Model.Teacher("Cypher"), ["s1", "s2", "s8"], True)
-    l7 = Model.Lesson("l7", "15:00", Model.Teacher("Brimstone"), ["s1", "s2", "s3"], True)
-    l8 = Model.Lesson("l8", "8:30", Model.Teacher("Brimstone"), ["s17", "s2", "s3"], False)
-    l9 = Model.Lesson("l9", "10:00", Model.Teacher("Raze"), ["s15", "s112", "s9"], True)
-    l10 = Model.Lesson("l10", "10:00", Model.Teacher("Skye"), ["s16", "s12", "s10"], False)
+    l1 = Model.Lesson("l1", "none", Model.Teacher("Jett"), ["s2", "s222", "s3333"], True)
+    l2 = Model.Lesson("l2", "none", Model.Teacher("Brimstone"), ["s134", "s2", "s6"], False)
+    l3 = Model.Lesson("l3", "none", Model.Teacher("Raze"), ["s1", "s22", "s5"], True)
+    l4 = Model.Lesson("l4", "none", Model.Teacher("Sage"), ["s1", "s22", "s4"], False)
+    l5 = Model.Lesson("l5", "none", Model.Teacher("Sage"), ["s111", "s2", "s3"], True)
+    l6 = Model.Lesson("l6", "none", Model.Teacher("Cypher"), ["s1", "s2", "s8"], True)
+    l7 = Model.Lesson("l7", "none", Model.Teacher("Brimstone"), ["s1", "s2", "s3"], True)
+    l8 = Model.Lesson("l8", "none", Model.Teacher("Brimstone"), ["s17", "s2", "s3"], False)
+    l9 = Model.Lesson("l9", "none", Model.Teacher("Raze"), ["s15", "s112", "s9"], True)
+    l10 = Model.Lesson("l10", "none", Model.Teacher("Skye"), ["s16", "s12", "s10"], False)
     G = Model.Graph(["a", "b", "c", "d"])
     G.addEdge("a", "b")
     G.addEdge("b", "c")
